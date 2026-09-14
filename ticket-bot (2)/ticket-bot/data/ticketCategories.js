946d5da88f9fe6e6ee481ba605522953e8c28e56
hghgh57 <rizzbut3@gmail.com>
2026-09-14 20:23:01 +1000

    Clarify questions for Buy/Sell Spawner category
    Updated questions for the Buy/Sell Spawner category to clarify user permissions.

--- ticket-bot (2)/ticket-bot/data/ticketCategories.js
+++ ticket-bot (2)/ticket-bot/data/ticketCategories.js
@@ -28,6 +28,7 @@ module.exports = [
     description: 'Open this if you want to buy/sell spawners.',
     notes: 'Only buy/sell with users who have spawner perms.',
     questions: ['How many spawners are you buying/selling?'],
+     questions: ['Only buy/sell spawners with users who have spawner perms!'],
   },
   {
     id: 'giveaway_claim',
